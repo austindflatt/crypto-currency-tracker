202e3dc0209a90d5b245922717d1a21c555840c1
Austin Flatt <austindflatt@gmail.com>
2021-12-10 16:37:17 -0500

    all crypto coins are now listed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const watchArea = document.querySelector('#watch-area');
 const watchList = document.createElement('div');
 
 const showGraph = document.querySelector('#show-graph');
+const graphDisplay = document.createElement('div');
 
 const coinArea = document.querySelector('#coin-area');
 const coinList = document.createElement('div');
@@ -12,31 +13,57 @@ async function myFunction() {
     const data = await httpResponse.json();
     const coins = data.data;
 
-    for(let i = 0; i < coins.length; i++) {
-        const coinInfo = coins[i];
+    // loop through crypto coins
+    for(i = 0; i < coins.length; i++) {
+        coinInfo = coins[i];
         console.log(coinInfo);
-        const coinName = coinInfo.name;
-        const price = coinInfo.priceUsd;
-        const symbol = coinInfo.symbol;
-        const change = coinInfo.changePercent24Hr;
-        const marketCap = coinInfo.marketCapUsd;
-        const supply = coinInfo.supply;
-        coinList.innerHTML = `<div class="coin-detail">${symbol} ${coinName}, Last Price: ${price}, Market Cap: ${marketCap}, Supply: ${supply}, 24h Change: ${change} <button class="favorite">Add to watchlist</button></div>`
+        coinName = coinInfo.name;
+        price = coinInfo.priceUsd;
+        symbol = coinInfo.symbol;
+        change = coinInfo.changePercent24Hr;
+        marketCap = coinInfo.marketCapUsd;
+        supply = coinInfo.supply;
+        const singleCoin = `<div class="coin-detail">
+        ${coins[i].symbol} ${coins[i].name}, 
+        Last Price: $${coins[i].priceUsd}, 
+        Market Cap: ${coins[i].marketCapUsd}, 
+        Supply: ${coins[i].supply}, 
+        24h Change: ${coins[i].changePercent24Hr}
+        <button class="graph">Show graph</button>
+        <button class="favorite">Add to watchlist</button>
+        </div>`
+        coinList.innerHTML += singleCoin
         coinArea.append(coinList);
     }
-    for(i = 0; i < 20; i++) {
-        /* coinList.innerHTML = `<div class="coin-detail">${symbol}, Name: ${coinArea}, Price: ${price}, Market Cap: ${marketCap}, Supply: ${supply}, Change: ${change}</div>`
-        coinArea.append(coinList); */
-    }
+
     // add to watchlist
     const watchListClick = document.querySelectorAll('.favorite');
     watchListClick.forEach(element =>
         element.addEventListener('click', function() {
-            console.log('added to watchlist')
-            watchList.innerHTML = `info added to watchlist`;
+            console.log('added to watchlist');
+            watchList.innerHTML = `
+            <div class="watch-box">${coinName}
+            <br />
+            $${price}
+            </div>
+            `;
             watchArea.append(watchList);
         })
         )
+
+    // show graph
+    const graphClick = document.querySelectorAll('.graph');
+    graphClick.forEach(element =>
+        element.addEventListener('click', function() {
+            console.log('this will show the graph!');
+            graphDisplay.innerHTML = `
+            <div class="graph-box"><canvas id="myChart" width="400" height="400"></canvas></div>
+            `;
+            showGraph.append(graphDisplay);
+        })
+        )
+    
+    // graph data
 }
 
 myFunction();
