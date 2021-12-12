1c820a544b7da416ca96d110aa44821cf97c2d7c
Austin Flatt <austindflatt@gmail.com>
2021-12-11 20:12:56 -0500

    users can now see each crypto currency icon and added dark/light mode

--- main.js
+++ main.js
@@ -23,14 +23,36 @@ async function myFunction() {
         change = coinInfo.changePercent24Hr;
         marketCap = coinInfo.marketCapUsd;
         supply = coinInfo.supply;
+        coinIcon = symbol.toLowerCase();
         const singleCoin = `<div class="coin-detail">
-        ${coins[i].symbol} ${coins[i].name}, 
-        Last Price: $${coins[i].priceUsd}, 
-        Market Cap: ${coins[i].marketCapUsd}, 
-        Supply: ${coins[i].supply}, 
-        24h Change: ${coins[i].changePercent24Hr}
-        <button class="graph">Show graph</button>
-        <button class="favorite">Add to watchlist</button>
+        <img src="https://assets.coincap.io/assets/icons/${coinIcon}@2x.png" class="icon">
+        <div class="symbol-name">
+        ${coins[i].symbol} ${coins[i].name}
+        </div>
+        <div class="last-price">
+        Last Price: $${coins[i].priceUsd}
+        </div>
+        <div class="market-cap">
+        Market Cap: ${coins[i].marketCapUsd}
+        </div>
+        <div class="supply">
+        Supply: ${coins[i].supply}
+        </div>
+        <div class="price-change">
+        <span class="positive">24h Change: ${coins[i].changePercent24Hr}</span>
+        </div>
+        <div class="coin-buttons">
+        <button class="graph" onclick="window.scrollTo(0, 0);">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
+            </svg>
+        </button>
+        <button class="favorite">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+            </svg>
+        </button>
+        </div>
         </div>`
         coinList.innerHTML += singleCoin
         coinArea.append(coinList);
@@ -42,28 +64,61 @@ async function myFunction() {
         element.addEventListener('click', function() {
             console.log('added to watchlist');
             watchList.innerHTML = `
-            <div class="watch-box">${coinName}
+            <div class="watch-box">
+            ${symbol}
+            ${coinName}
             <br />
             $${price}
+            24h Change: ${change}
             </div>
             `;
             watchArea.append(watchList);
         })
-        )
+        );
 
     // show graph
     const graphClick = document.querySelectorAll('.graph');
     graphClick.forEach(element =>
         element.addEventListener('click', function() {
+            window.scrollTo(0, 0);
             console.log('this will show the graph!');
             graphDisplay.innerHTML = `
-            <div class="graph-box"><canvas id="myChart" width="400" height="400"></canvas></div>
+            <div class="graph-box">
+                <canvas id="myChart" width="400" height="400"></canvas>
+            </div>
             `;
             showGraph.append(graphDisplay);
         })
-        )
+        );
     
     // graph data
-}
+};
 
 myFunction();
+
+// dark mode //
+let darkMode = localStorage.getItem('darkMode'); 
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+function enableDarkMode() {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+function disableDarkMode() {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null);
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', function() {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {  
+        disableDarkMode(); 
+    }
+});
